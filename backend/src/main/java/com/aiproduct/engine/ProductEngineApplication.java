1d7a0e8437c9fbf74c13adfd3f65707dab1746c9
anagha2145 <anagha21042005@gmail.com>
2026-08-22 14:04:34 +0530

    Connect dashboard to Spring Boot

---
+++ backend/src/main/java/com/aiproduct/engine/ProductEngineApplication.java
@@ -0,0 +1,12 @@
+package com.aiproduct.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductEngineApplication.class, args);
+    }
+}
